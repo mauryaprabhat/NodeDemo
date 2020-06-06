995b81b7621bc576ec3ffd49df46c147ab79a8a0
Maurya <Prabhat.Maurya@allscripts.com>
2020-06-06 18:07:44 +0530

    corrected the PORT keyword in the package json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server is started at ${port}`));
 
 const courses = [
